7f1dd8bab2665f300816ba832bb4cf413309d5cd
Roberto Carvajal <robertocarvajalvelasco@gmail.com>
2022-12-14 20:37:14 +0100

    Commit para arreglar gitignore

--- kilosapi/src/main/java/com/grupocinco/kilosapi/MainDePruebas.java
+++ kilosapi/src/main/java/com/grupocinco/kilosapi/MainDePruebas.java
@@ -1,6 +1,10 @@
 package com.grupocinco.kilosapi;
 
+import com.grupocinco.kilosapi.model.Destinatario;
+import com.grupocinco.kilosapi.repository.CajaRepository;
+import com.grupocinco.kilosapi.repository.DestinatarioRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.PostConstruct;
@@ -8,9 +12,26 @@ import javax.annotation.PostConstruct;
 @Component
 @RequiredArgsConstructor
 public class MainDePruebas {
+    @Autowired
+    private DestinatarioRepository repoDestinatario;
+    @Autowired
+    CajaRepository repoCaja;
+    @Autowired
+
     @PostConstruct
     public void datos(){
-        //AÑADIR DATOS AQUÍ
+        Destinatario des1 = Destinatario.builder()
+                                        .nombre("Comedor Pagés del Corro")
+                                        .direccion("C/ Pagés del Corro 34")
+                                        .personaContacto("María")
+                                        .telefono("954347087")
+                                        .build();
+        Destinatario des2 = Destinatario.builder()
+                                        .nombre("Hermanitas de los Pobres")
+                                        .direccion("C/ Luis Montoto 43")
+                                        .personaContacto("José")
+                                        .telefono("954543092")
+                                        .build();
     }
 
 }
