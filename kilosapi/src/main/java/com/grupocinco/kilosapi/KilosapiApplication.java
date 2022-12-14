d1c693fc19fb9ea6ae42b70e33c09f49af8adeec
Roberto Carvajal <carvajal.verob22@triana.salesianos.edu>
2022-12-14 10:56:13 +0100

    First commit w/default spring app

---
+++ kilosapi/src/main/java/com/grupocinco/kilosapi/KilosapiApplication.java
@@ -0,0 +1,13 @@
+package com.grupocinco.kilosapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KilosapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KilosapiApplication.class, args);
+	}
+
+}
---
+++ kilosapi/src/test/java/com/grupocinco/kilosapi/KilosapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grupocinco.kilosapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KilosapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
