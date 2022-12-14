b9e9bc8075b2260ce7aa69a51fe2cf637a164e22
AlejandroDV <aflowv@hotmail.com>
2022-12-14 18:27:35 +0100

    Errores lineas comentadas

--- kilosapi/src/main/java/com/grupocinco/kilosapi/model/KilosDisponibles.java
+++ kilosapi/src/main/java/com/grupocinco/kilosapi/model/KilosDisponibles.java
@@ -10,7 +10,8 @@ public class KilosDisponibles {
     @Id
     @ManyToOne
     @JoinColumn(name="tipoAlimento_id", foreignKey = @ForeignKey(name = "FK_KILOSDISPONIBLES_TIPOALIMENTO"))
-    private TipoAlimento tipoAlimento;
+    //Hay que cambiarlo por TipoAlimento en vez de Long
+    private Long tipoAlimento;
     private Double cantidadDisponible;
 
 }
--- kilosapi/src/main/java/com/grupocinco/kilosapi/repository/KilosDisponiblesRepository.java
+++ kilosapi/src/main/java/com/grupocinco/kilosapi/repository/KilosDisponiblesRepository.java
@@ -3,5 +3,6 @@ package com.grupocinco.kilosapi.repository;
 import com.grupocinco.kilosapi.model.KilosDisponibles;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface KilosDisponiblesRepository extends JpaRepository<TipoAlimento, KilosDisponibles> {
+//Hay que cambiarlo por TipoAlimento en vez de Long
+public interface KilosDisponiblesRepository extends JpaRepository<Long, KilosDisponibles> {
 }
