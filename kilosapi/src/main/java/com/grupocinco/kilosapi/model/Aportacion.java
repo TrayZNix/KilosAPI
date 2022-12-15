e8b4f49ed9e41391537bf2625e77d1cad9765243
AlejandroDV <aflowv@hotmail.com>
2022-12-15 22:07:13 +0100

    Intento de avance en entidades

--- kilosapi/src/main/java/com/grupocinco/kilosapi/model/Aportacion.java
+++ kilosapi/src/main/java/com/grupocinco/kilosapi/model/Aportacion.java
@@ -6,9 +6,9 @@ import javax.persistence.*;
 import java.util.ArrayList;
 import java.util.List;
 
-@Entity
+@Entity @ToString
 @AllArgsConstructor @NoArgsConstructor
-@Setter @Getter @ToString
+@Setter @Getter
 @Builder
 public class Aportacion {
     @Id @GeneratedValue
@@ -16,28 +16,23 @@ public class Aportacion {
 
     private String fecha;
 
-//    @ManyToOne
-//    @JoinColumn(name="clase_id", foreignKey = @ForeignKey(name = "FK_APORTACION_CLASE"))
-//    private Clase clase;
+   @ManyToOne
+   @JoinColumn(name="clase_id", foreignKey = @ForeignKey(name = "FK_APORTACION_CLASE"))
+   private Clase clase;
+
+   @ToString.Exclude
+   @OneToMany(mappedBy = "detalleAportacionId", fetch = FetchType.EAGER, cascade = CascadeType.ALL, orphanRemoval = true)
+   @Builder.Default
+   private List<DetalleAportacion> detalles = new ArrayList<>();
+
+
+//   public void addToClase(Clase c) {
+//       this.clase = c;
+//       c.getAportacion().add(this);
+//   }
 //
-//    @ToString.Exclude
-//    @OneToMany(mappedBy = "aportacion", fetch = FetchType.EAGER)
-//    @Builder.Default
-//    private List<DetalleAportacion> detalles = new ArrayList<>();
-//
-//
-//
-//    @PreRemove
-//    public void setNullDetalles(){
-//        detalles.forEach(a ->a.setAportacion(null));
-//    }
-//    public void addToClase(Clase c) {
-//        this.clase = c;
-//        c.getAportacion().add(this);
-//    }
-//
-//    public void removeFromClase(Clase c) {
-//        this.clase = null;
-//        c.getAportacion().remove(this);
-//    }
+//   public void removeFromClase(Clase c) {
+//       this.clase = null;
+//       c.getAportacion().remove(this);
+//   }
 }
--- kilosapi/src/main/java/com/grupocinco/kilosapi/model/KilosDisponibles.java
+++ kilosapi/src/main/java/com/grupocinco/kilosapi/model/KilosDisponibles.java
@@ -1,23 +1,25 @@
 package com.grupocinco.kilosapi.model;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 import javax.persistence.*;
+import java.io.Serializable;
 
-@Entity
-@AllArgsConstructor
-@NoArgsConstructor
-@Getter
-@Setter
-public class KilosDisponibles {
-
+@Entity @ToString
+@AllArgsConstructor @NoArgsConstructor
+@Getter @Setter
+public class KilosDisponibles implements Serializable {
     @Id
-    @ManyToOne
-    @JoinColumn(name="tipoAlimento", foreignKey = @ForeignKey(name = "FK_KILOSDISPONIBLES_TIPOALIMENTO"))
-    private TipoAlimento tipoAlimento;
+    @ManyToOne @JoinColumn(name="tipoAlimento_id", foreignKey = @ForeignKey(name = "FK_KILOSDISPONIBLES_TIPOALIMENTO"))
+    private TipoAlimento  tipoAlimento;
     private Double cantidadDisponible;
 
+    public void addToTipoAlimento(TipoAlimento a){
+        this.tipoAlimento = a;
+        a.getKilosDisponible().add(this);
+    }
+    public void removeFromTipoAlimento(TipoAlimento a){
+        this.tipoAlimento = null;
+        a.getKilosDisponible().remove(this);
+    }
 }
