dcaab2cb540e54954c3030ef66cfc6ab7ab22527
Roberto Carvajal <robertocarvajalvelasco@gmail.com>
2022-12-17 18:43:10 +0100

    HOTFIX-Cambio de nombre erroneo de paquete controller

--- kilosapi/src/main/java/com/grupocinco/kilosapi/controller/DestinatarioController.java
+++ kilosapi/src/main/java/com/grupocinco/kilosapi/controller/DestinatarioController.java
@@ -20,6 +20,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
