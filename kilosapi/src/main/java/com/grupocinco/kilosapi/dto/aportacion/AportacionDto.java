bbd492974382c798cd5f0360be6d95a34afd0912
AlejandroDV <aflowv@hotmail.com>
2022-12-20 22:50:55 +0100

    Terminado

--- kilosapi/src/main/java/com/grupocinco/kilosapi/dto/aportacion/AportacionDto.java
+++ kilosapi/src/main/java/com/grupocinco/kilosapi/dto/aportacion/AportacionDto.java
@@ -12,27 +12,30 @@ import java.util.ArrayList;
 import java.util.List;
 
 @Builder
-@AllArgsConstructor@NoArgsConstructor
-@Getter @Setter @ToString
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@ToString
 public class AportacionDto {
-    @JsonView({AportacionViews.ListaAportacion.class,AportacionViews.AportacionById.class} )
+    @JsonView({AportacionViews.ListaAportacion.class, AportacionViews.AportacionById.class})
     private Long id;
-    @JsonView({AportacionViews.ListaAportacion.class,AportacionViews.AportacionById.class})
+    @JsonView({AportacionViews.ListaAportacion.class, AportacionViews.AportacionById.class})
     private LocalDate fecha;
-    @JsonView({AportacionViews.ListaAportacion.class,AportacionViews.AportacionById.class})
+    @JsonView({AportacionViews.ListaAportacion.class, AportacionViews.AportacionById.class})
     private String nombreClase;
-    @JsonView({AportacionViews.ListaAportacion.class,AportacionViews.AportacionById.class})
+    @JsonView({AportacionViews.ListaAportacion.class, AportacionViews.AportacionById.class})
     private Double kilosTotales;
 
     @JsonView({AportacionViews.AportacionById.class})
     List<DetalleAportacionDto> detalleAportaciones;
 
 
-    public static AportacionDto of(Aportacion a){
+    public static AportacionDto of(Aportacion a) {
         double kgTotales = 0.0;
         List<DetalleAportacionDto> detList = new ArrayList<DetalleAportacionDto>();
 
-        for (DetalleAportacion det: a.getDetalles()) {
+        for (DetalleAportacion det : a.getDetalles()) {
             kgTotales += det.getCantidad_en_kgs();
 
             detList.add(new DetalleAportacionDto(
@@ -40,7 +43,6 @@ public class AportacionDto {
                     det.getTipoAlimento().getNombre(),
                     det.getCantidad_en_kgs()));
         }
-
 
 
         return AportacionDto.builder()
--- kilosapi/src/main/java/com/grupocinco/kilosapi/model/Aportacion.java
+++ kilosapi/src/main/java/com/grupocinco/kilosapi/model/Aportacion.java
@@ -8,41 +8,42 @@ import java.util.ArrayList;
 import java.util.List;
 
 @Entity
-@AllArgsConstructor @NoArgsConstructor
-@Setter @Getter @ToString
+@AllArgsConstructor
+@NoArgsConstructor
+@Setter
+@Getter
+@ToString
 @Builder
 public class Aportacion {
-    @Id @GeneratedValue
+    @Id
+    @GeneratedValue
     private Long id;
 
     private LocalDate fecha;
 
-   @ManyToOne
-   @JoinColumn(name="clase_id", foreignKey = @ForeignKey(name = "FK_APORTACION_CLASE"))
-   private Clase clase;
+    @ManyToOne
+    @JoinColumn(name = "clase_id", foreignKey = @ForeignKey(name = "FK_APORTACION_CLASE"))
+    private Clase clase;
+    @ToString.Exclude
+    @OneToMany(mappedBy = "aportacion", fetch = FetchType.EAGER, cascade = CascadeType.ALL, orphanRemoval = true)
+    @Builder.Default
+    private List<DetalleAportacion> detalles = new ArrayList<DetalleAportacion>();
+    public void addToClase(Clase c) {
+        this.clase = c;
+        c.getAportaciones().add(this);
+    }
+    public void removeFromClase(Clase c) {
+        this.clase = null;
+        c.getAportaciones().remove(this);
+    }
 
-   @ToString.Exclude
-   @OneToMany(mappedBy = "aportacion", fetch = FetchType.EAGER, cascade = CascadeType.ALL, orphanRemoval = true)
-   @Builder.Default
-   private List<DetalleAportacion> detalles = new ArrayList<DetalleAportacion>();
+    public void addDetalleAportacion(DetalleAportacion d) {
+        this.getDetalles().add(d);
+        d.setAportacion(this);
+    }
 
-
-   public void addToClase(Clase c) {
-       this.clase = c;
-       c.getAportaciones().add(this);
-   }
-
-   public void removeFromClase(Clase c) {
-       this.clase = null;
-       c.getAportaciones().remove(this);
-   }
-
-   public void addDetalleAportacion(DetalleAportacion d){
-       this.getDetalles().add(d);
-       d.setAportacion(this);
-   }
-   public void removeDetalleAportacion(DetalleAportacion d){
-       this.getDetalles().remove(d);
-       d.setAportacion(null);
-   }
+    public void removeDetalleAportacion(DetalleAportacion d) {
+        this.getDetalles().remove(d);
+        d.setAportacion(null);
+    }
 }
